282e5c8b9128dc2712441621dbffe31fabdcd81e
Alexandru Scanteie <alexscanteie0808@gmail.com>
2025-02-10 20:09:53 +0200

    Create Tauri app

---
+++ tauri/src/main.js
@@ -0,0 +1,12 @@
+const { invoke } = window.__TAURI__.core;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+});
